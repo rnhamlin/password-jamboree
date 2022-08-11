e5815b0a2ce17fc5269a9f1b796e7149a017569e
reneenh <reneehamlinteaches@gmail.com>
2022-08-11 15:33:09 -0700

    added an alert to nudge users to select at least one character type if they say no to everything. added alert to prompt users to enter the right numbers if they enter a number less than 8 or greater than 128.

--- script.js
+++ script.js
@@ -23,6 +23,13 @@ function generatePassword() {
   // What if they do less than 8 and greater than 128? What if they enter a word?
 var passlength=prompt("How long would you like your password to be?")
 console.log(passlength)
+if(passlength<8) {
+  alert("Password must be at least 8 characters long.")
+}
+if(passlength>128) {
+  alert("Password may not be more than 128 characters.")
+}
+// if(passlength=)
 var lowercase=confirm("Do you want lowercase characters?")
 console.log(possiblechars)
 if(lowercase) {
@@ -40,7 +47,12 @@ var spec=confirm("Do you want special characters?")
 if(spec) {
   possiblechars=possiblechars + spchar
 }
-// What if they answer "no" to everything?
+// What if they answer "no" to everything? --> Add if/else statement to alert user to select at least some character types to create a password.
+if(lowerchars, upperchars,  number, spchar=false)
+console.log(possiblechars=false)
+else {
+  alert("At least one character type must be selected for a password to be generated.")
+}
 console.log(possiblechars)
 console.log(possiblechars[2])
 for(i=0; i < passlength; i++) {
