7f8ccf84127c1280143acae1bbcebbc1c7a04125
reneenh <reneehamlinteaches@gmail.com>
2022-08-12 20:22:47 -0700

    Fixed code bugs--eliminated error msg alerts and had that message appear in the text box.

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function writePassword() {
   var password = generatePassword(); 
   console.log(password)
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 
@@ -21,50 +19,52 @@ function generatePassword() {
   var spchar="!@#$%^&*()"
   var password=""
   // What if they do less than 8 and greater than 128? What if they enter a word?
-var passlength=prompt("How long would you like your password to be?")
-console.log(passlength)
-if(passlength<8) {
-  alert("Password must be at least 8 characters long.")
-}
-if(passlength>128) {
-  alert("Password may not be more than 128 characters.")
-}
-// if(passlength=)
-var lowercase=confirm("Do you want lowercase characters?")
-console.log(possiblechars)
-if(lowercase) {
-  possiblechars=possiblechars + lowerchars
-}
-var uppercase=confirm("Do you want uppercase letters?")
-if(uppercase) {
-  possiblechars=possiblechars + upperchars
-}
-var num=confirm("Do you want numbers?")
-if(num) {
-  possiblechars=possiblechars + number
-}
-var spec=confirm("Do you want special characters?")
-if(spec) {
-  possiblechars=possiblechars + spchar
-}
-// What if they answer "no" to everything? --> Add if/else statement to alert user to select at least some character types to create a password.
-if(lowerchars, upperchars,  number, spchar=false)
-console.log(possiblechars=false)
-else {
-  alert("At least one character type must be selected for a password to be generated.")
-}
-console.log(possiblechars)
-console.log(possiblechars[2])
-for(i=0; i < passlength; i++) {
-  var randomIndex=Math.floor(Math.random()*possiblechars.length)
-  console.log(i)
-  console.log(randomIndex)
-  console.log(possiblechars[randomIndex])
-  password+=possiblechars[randomIndex]
-  console.log(password)
+  var passlength=prompt("How long would you like your password to be?")
+  console.log(passlength)
+  if(passlength<8) {
+    // alert("Password must be at least 8 characters long.")
+    return "Password must be at least 8 characters long."
+  }
+  if(passlength>128) {
+    // alert("Password must not be more than 128 characters.")
+    return "Password must not be more than 128 characters."
+  }
+  // if(passlength=)
+  var lowercase=confirm("Do you want lowercase characters?")
+  console.log(possiblechars)
+  if(lowercase) {
+    possiblechars=possiblechars + lowerchars
+  }
+  var uppercase=confirm("Do you want uppercase letters?")
+  if(uppercase) {
+    possiblechars=possiblechars + upperchars
+  }
+  var num=confirm("Do you want numbers?")
+  if(num) {
+    possiblechars=possiblechars + number
+  }
+  var spec=confirm("Do you want special characters?")
+  if(spec) {
+    possiblechars=possiblechars + spchar
+  }
+  // What if they answer "no" to everything? --> Add if/else statement to alert user to select at least some character types to create a password.
+  if(lowerchars, upperchars,  number, spchar=false)
+  console.log(possiblechars=false)
+  else {
+    alert("At least one character type must be selected for a password to be generated.")
+  }
+  console.log(possiblechars)
+  console.log(possiblechars[2])
+  for(i=0; i < passlength; i++) {
+    var randomIndex=Math.floor(Math.random()*possiblechars.length)
+    console.log(i)
+    console.log(randomIndex)
+    console.log(possiblechars[randomIndex])
+    password+=possiblechars[randomIndex]
+    console.log(password)
 
-}
-return password
+  }
+  return password
 }
 
 // Add event listener to generate button
